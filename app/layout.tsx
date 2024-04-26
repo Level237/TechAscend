39d5a0470218a5e481715bc0ab85dd85c9aff679
level237 <Bramslevel129@gmail.com>
2024-04-26 13:09:52 +0100

    header prototype

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-gradient-to-r from-blue-300 to-slate-50'>{children}</body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react'
+import { Button } from './ui/button'
+
+export default function Header() {
+  return (
+    <header className="w-[100%] sticky  z-[30] bg-transparent backdrop-blur   border-b-[0.1px] dark:border-[#e8d1bb38] border-[#5134173a] top-0  py-4 px-4 sm:px-10 dark:bg-transparent  bg-[#003366]  font-[sans-serif] min-h-[90px]">
+      <section className="flex fle flex-wrap items-center gap-x-2 max-lg:gap-y-6 justify-between">
+      <div className="w-[180px]">
+            <Image 
+            src="/logo.png"
+            width="60"
+            height="60"
+            alt="logo agency cameroon"
+            />
+      
+        </div>
+        <div className='flex-1'>
+      <ul className='flex text-white text-[1rem] gap-[3.5rem] cursor-pointer items-center'>
+        <li>Home</li>
+        <li>Community</li>
+        <li>Programs</li>
+        <li>Career Readliness</li>
+        <li>About us</li>
+        <li>Contact Us</li>
+        <li>
+          <Button className='bg-transparent text-base border-white border-[1px] rounded-2xl px-7' >Get Started</Button>
+        </li>
+        <li>Login</li>
+      </ul>
+       </div>
+      </section>
+    </header>
+  )
+}
