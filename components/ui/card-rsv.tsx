6f1ebc89c99dc7846d9038e292a1add2ef1ba80e
level237 <Bramslevel129@gmail.com>
2024-04-27 19:14:30 +0100

    redux in action

--- components/ui/card-rsv.tsx
+++ components/ui/card-rsv.tsx
@@ -4,13 +4,17 @@ import { Button } from './button'
 import { X } from 'lucide-react'
 import {  useDispatch, useSelector } from 'react-redux';
 import { hidden } from '@/store/store';
+import { AnimatePresence,motion } from 'framer-motion'
 
 export default function CardRsv() {
   const isVisible = useSelector((state:any) => state.RsvpSlice.isVisible);
   const dispatch = useDispatch();
   return (
     <>
-    {isVisible && (<div className='bg-[#003366] h-[87px] mt-12  flex justify-between px-10 items-center gap-3 '>
+    <AnimatePresence>
+    {isVisible && (<motion.div
+    exit={{ opacity:0, transition: {duration: 0.5}}}
+    className='bg-[#003366] h-[87px] mt-12  flex justify-between px-10 items-center gap-3 '>
       <div className='flex justify-center items-center text-white gap-16'>
         <div>
           <span>Unlock Tech Riches in 2024</span>
@@ -28,7 +32,8 @@ export default function CardRsv() {
       <div className='text-white cursor-pointer'>
         <X onClick={() => dispatch(hidden())}/>
       </div>
-    </div>)}
+    </motion.div>)}
+    </AnimatePresence>
      </>
    
   )
