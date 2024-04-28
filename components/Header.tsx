f32a17b24c492964492757318ef3791549039fc5
level237 <Bramslevel129@gmail.com>
2024-04-28 14:14:34 +0100

    header mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   const dispatch = useDispatch();
   return (
     <>
-    <header className="w-[100%] fixed   z-[30]    border-b-[0.1px]  border-[#5134173a] top-0  py-4 px-4 sm:px-10 dark:bg-transparent  bg-[#003366]  font-[sans-serif] min-h-[90px]">
+    <header className="w-[100%] fixed   z-[900]    border-b-[0.1px]  border-[#5134173a] top-0  py-4 px-4 sm:px-10 dark:bg-transparent  bg-[#003366]  font-[sans-serif] min-h-[90px]">
       <section className="flex  flex-wrap items-center  max-lg:gap-y-6 justify-between">
       <div className="w-[180px]">
         <Link href={'/'}>
@@ -87,6 +87,61 @@ export default function Header() {
           </button>
        </div>
       </section>
+    </header>
+    <header className='bg-[#003366] z-[20]    fixed top-[1rem] h-full w-full left-0'>
+    <section className="flex absolute  left-0 h-full w-full flex-wrap mt-[6rem] max-lg:gap-y-6 justify-center">
+    <ul className='flex flex-col   text-white text-[1rem] gap-[1rem] cursor-pointer items-center'>
+        <li className='group' ><MenuLink title={'home'} link={''} isCaret={false}/></li>
+        <li><MenuLink title={'Community'} link={''} isCaret={true} sub={[{
+          title: 'Skill Workshops', link: 'https://tech-ascend.com/community/skill-Workshops',
+          isBlank: 1
+        },{
+          title: 'Networking Events', link: 'community/networking-events',
+          isBlank: 0
+        },{
+          title: 'Job Fairs', link: 'https://tech-ascend.com/community/job-fairs',
+          isBlank: 1
+        }]}/></li>
+        <li><MenuLink title={'Programs'} link={''} isCaret={true} sub={[{
+          title: 'Frontend', link: 'https://tech-ascend.com/programs/front-end',
+          isBlank: 1
+        },{
+          title: 'Backend', link: 'https://tech-ascend.com/programs/back-end',
+          isBlank: 1
+        },{
+          title: 'DevOps', link: 'https://tech-ascend.com/programs/devops',
+          isBlank: 1
+        },{
+          title: 'Data Science', link: 'https://tech-ascend.com/programs/data-science',
+          isBlank: 1
+        }]}/></li>
+        <li> <MenuLink title={'Career Readliness'} link={''} isCaret={true} sub={[{
+          title: 'Jobs', link: 'https://tech-ascend.com/career/jobs',
+          isBlank: 1
+        },{
+          title: 'Mentorship Programs', link: 'https://tech-ascend.com/career/mentorship-programs',
+          isBlank:1
+        },{
+          title: 'Career Placement', link: 'https://tech-ascend.com/career/career-placement',
+          isBlank: 1
+        }]}/></li>
+        <li> <MenuLink title={'About us'} link={''} isCaret={false}/></li>
+        <li> <MenuLink title={'Contact Us'} link={''} isCaret={false}/></li>
+        <li>
+          <Button onClick={()=>dispatch(open())} className='bg-transparent text-base border-white border-[1px] rounded-2xl px-7' >Get Started</Button>
+        </li>
+        <li><MenuLink title={'Login'} link={''} isCaret={true} sub={[{
+          title: 'For Clients', link: '',
+          isBlank: 0
+        },{
+          title: 'For Talents', link: '',
+          isBlank: 0
+        },{
+          title: 'For Students', link: '',
+          isBlank: 0
+        }]}/></li>
+      </ul>
+</section>
     </header>
     </>
     
